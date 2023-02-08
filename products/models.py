7c3ed6370dc73f2b76c2cdeeeb4085d1508effbe
zainjesus <1337zain1337@mail.ru>
2023-02-08 06:11:51 +0600

    models

--- products/models.py
+++ products/models.py
@@ -15,43 +15,40 @@ class Category(models.Model):
 
 
 class House(models.Model):
-    image = models.ImageField(max_length=50)
+    image = models.ImageField(null=True)
     price = models.IntegerField()
     description = models.TextField()
     category = models.ManyToManyField(Category)
-    rooms_count = (
+    rooms = models.CharField(default='3', choices=(
         ('1', '1'),
         ('2', '2'),
         ('3', '3'),
         ('4', '4'),
-        ('5', '5'),
-    )
-    rooms = models.CharField(default='3', choices=rooms_count, max_length=50)
-    floor_count = (
+        ('5', '5')
+    ), max_length=50)
+    floor = models.CharField(default='3', choices=(
         ('1', '2'),
         ('2', '2'),
         ('3', '3'),
         ('4', '4')
-    )
-    floor = models.CharField(default='3', choices=floor_count, max_length=50)
+    ), max_length=50)
 
     def __str__(self):
         return f'{self.category}'
 
 
 class Apartment(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(null=True)
     price = models.IntegerField()
     description = models.TextField()
     category = models.ManyToManyField(Category)
-    rooms_count = (
+    rooms = models.CharField(default='3', choices=(
         ('1', '2'),
         ('2', '2'),
         ('3', '3'),
         ('4', '4'),
-        ('5', '5'),
-    )
-    rooms = models.CharField(default='3', choices=rooms_count, max_length=50)
+        ('5', '5')
+    ), max_length=50)
 
     def __str__(self):
         return f'{self.category}'
